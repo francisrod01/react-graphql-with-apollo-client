754e1b980b0b31e7e0e82c750a49b960dd0c0f4d
Francis Rodrigues <francis.rod01@outlook.com>
2019-02-15 15:28:24 -0300

    Added apollo welcome message in App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to Apollo!</h2>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
