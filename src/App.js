c132bd07cf6b56591dfccf3b402d1e5ede89c191
Francis Rodrigues <francis.rod01@outlook.com>
2019-02-15 16:33:09 -0300

    Implemented a graphQL data fetch using apollo client

--- src/App.js
+++ src/App.js
@@ -1,34 +1,68 @@
 import React, { Component } from 'react';
+import { ApolloClient } from 'apollo-boost';
+import gql from 'graphql-tag';
+import {
+  graphql,
+  ApolloProvider
+} from 'react-apollo';
+
 import logo from './logo.svg';
 import './App.css';
 
-const ChannelsList = () => (
-  <ul className="Item-list">
-    <li>Channel 1</li>
-    <li>Channel 2</li>
-  </ul>
-);
+
+const apolloClient = new ApolloClient();
+
+const channelsListQuery = gql`
+  query ChannelsListQuery {
+    channels {
+      id
+      name
+    }
+  }
+`;
+
+const ChannelsList = ({ data: { loading, error, channels } }) => {
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>{error.message}</p>;
+  }
+
+  return (
+    <ul className="Item-list">
+      {
+        channels.map(ch => <li key={ch.id}>{ch.name}</li>)
+      }
+    </ul>
+  );
+}
+
+const ChannelsListWithData = graphql(channelsListQuery)(ChannelsList);
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Apollo!</h2>
+      <ApolloProvider client={apolloClient}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to Apollo!</h2>
 
-          <ChannelsList />
+            <ChannelsListWithData />
 
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </ApolloProvider>
     );
   }
 }
