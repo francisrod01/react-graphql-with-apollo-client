6631e809a5476ab02ecf8d8613a13fa2cf1652e2
Francis Rodrigues <francis.rod01@outlook.com>
2019-04-05 21:33:56 -0300

    [server] Setting up GrapiQL tool

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 import express from "express";
-import { graphqlExpress } from "graphql-server-express";
+import {
+  graphqlExpress,
+  graphiqlExpress
+} from "graphql-server-express";
 import bodyParser from "body-parser";
 
 import { schema } from "./schema";
@@ -8,5 +11,7 @@ const PORT = process.env.PORT || 4000;
 const server = express();
 
 server.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+
+server.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
 server.listen(PORT, () => console.log(`Express Server is running on http://localhost:${PORT}`));
