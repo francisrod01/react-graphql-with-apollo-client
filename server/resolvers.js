4ecf776f727691354572b317c6db331a480b87bf
Francis Rodrigues <francis.rod01@outlook.com>
2019-04-09 19:22:13 -0300

    [server] Changed all ids to string in resolvers

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,28 +1,28 @@
 const channels = [
   {
-    id: 1,
+    id: "1",
     name: 'soccer',
     messages: [
       {
-        id: 1,
+        id: "1",
         text: 'soccer if football'
       },
       {
-        id: 2,
+        id: "2",
         text: 'hello soccer world cup!'
       }
     ]
   },
   {
-    id: 2,
+    id: "2",
     name: 'baseball',
     messages: [
       {
-        id: 3,
+        id: "3",
         text: 'baseball is life'
       },
       {
-        id: 4,
+        id: "4",
         text: 'hello baseball world series!'
       }
     ]
@@ -42,7 +42,7 @@ export const resolvers = {
   },
   Mutation: {
     addChannel: (root, args) => {
-      const newChannel = { id: nextId++, messages: [], name: args.name };
+      const newChannel = { id: String(nextId++), messages: [], name: args.name };
       channels.push(newChannel);
       return newChannel;
     },
