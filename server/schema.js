f853a9876949a6a6f64b6c11fc749147ed8d2387
Francis Rodrigues <francis.rod01@outlook.com>
2019-04-09 18:43:34 -0300

    [server] Added channel messages to schema

--- server/schema.js
+++ server/schema.js
@@ -5,13 +5,20 @@ import { resolvers } from "./resolvers";
 const typeDefs = `
   type Channel {
     id: ID!   ## "!" denotes a required field
-    name: String
+    name: String,
+    messages: [Message]!
+  }
+
+  type Message {
+    id: ID!
+    text: String
   }
 
   # This types specifies the entry points into our API. In this case
   # there is only one - "channels" - which returns a list of channels
   type Query {
     channels: [Channel]   ## "[]" means this is a list of channels
+    channel(id: ID!): Channel
   }
 
   # The mutation root type, used to define all mutations.
