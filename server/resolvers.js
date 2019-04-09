db66628048ae401c8cf23e3ae862b1bd93b28c6b
Francis Rodrigues <francis.rod01@outlook.com>
2019-04-09 18:43:47 -0300

    [server] Added channel messages to resolvers

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,11 +1,31 @@
 const channels = [
   {
     id: 1,
-    name: 'soccer'
+    name: 'soccer',
+    messages: [
+      {
+        id: 1,
+        text: 'soccer if football'
+      },
+      {
+        id: 2,
+        text: 'hello soccer world cup!'
+      }
+    ]
   },
   {
     id: 2,
-    name: 'baseball'
+    name: 'baseball',
+    messages: [
+      {
+        id: 3,
+        text: 'baseball is life'
+      },
+      {
+        id: 4,
+        text: 'hello baseball world series!'
+      }
+    ]
   }
 ];
 
@@ -16,10 +36,13 @@ export const resolvers = {
     channels: () => {
       return channels;
     },
+    channel: (root, { id }) => {
+      return channels.find(channel => channel.id === id);
+    },
   },
   Mutation: {
     addChannel: (root, args) => {
-      const newChannel = { id: nextId++, name: args.name };
+      const newChannel = { id: nextId++, messages: [], name: args.name };
       channels.push(newChannel);
       return newChannel;
     },
