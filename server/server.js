f1012d8198539536807108e2b1e62162a5479fa3
Francis Rodrigues <francis.rod01@outlook.com>
2019-04-10 16:30:07 -0300

    [server] Added websocket transport for subscriptions

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 import express from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
+
+import { createServer } from "http";
+import { execute, subscribe } from "graphql";
 import {
   graphqlExpress,
   graphiqlExpress
 } from "graphql-server-express";
-import bodyParser from "body-parser";
+import { SubscriptionServer } from "subscriptions-transport-ws";
 
 import { schema } from "./schema";
 
@@ -16,6 +20,24 @@ server.use("*", cors({ origin: "http://localhost:3000" }));
 
 server.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 
-server.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+server.use('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql',
+  subscriptionsEndpoint: 'ws://localhost:4000/subscriptions'
+}));
 
-server.listen(PORT, () => console.log(`Express Server is running on http://localhost:${PORT}`));
+// Wrap the Express server
+const ws = createServer(server);
+
+ws.listen(PORT, () => {
+  console.log(`GraphQL Server is running on http://localhost:${PORT}`);
+
+  // Set up the WebSocket for handling GraphQL subscriptions.
+  new SubscriptionServer({
+    execute,
+    subscribe,
+    schema
+  }, {
+    server: ws,
+    path: "/subscriptions",
+  });
+});
