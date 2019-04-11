29d79c1c9ca24cd6f045a6ea54a9144193eef925
Francis Rodrigues <francis.rod01@outlook.com>
2019-04-11 13:51:34 -0300

    [server] Updated schema with messageFeed type

--- server/schema.js
+++ server/schema.js
@@ -6,12 +6,22 @@ const typeDefs = `
   type Channel {
     id: ID!   ## "!" denotes a required field
     name: String,
-    messages: [Message]!
+    messages: [Message]
+    # Messages will be returned in a MessageFeed object wrapper
+    messageFeed(cursor: String): MessageFeed
   }
 
   type Message {
     id: ID!
     text: String
+    createdAt: Int
+  }
+
+  type MessageFeed {
+    # Cursor specifies the place in the list where we left off
+    cursor: String!
+    # this is a chunk of messages to be returned
+    messages: [Message]!
   }
 
   # This types specifies the entry points into our API. In this case
